1e1610aad1f8401453693d0cfec1af32bd3f4456
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-12-22 01:12:53 +0300

    port change

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express();
-const port = 3000 || 5000;
+// const port = 3000 || 5000;
 
 app.get('/api', (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Example app listening!`)
 });
